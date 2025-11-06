4147fc78da97dce4a4887530c5f5191e6fc1a622
Peter Steinberger <steipete@gmail.com>
2025-11-06 01:51:09 +0000

    poltergeist 1.9.0

--- Formula/poltergeist.rb
+++ Formula/poltergeist.rb
@@ -1,9 +1,9 @@
 class Poltergeist < Formula
   desc "Universal file watcher with auto-rebuild for any language or build system"
   homepage "https://github.com/steipete/poltergeist"
-  url "https://github.com/steipete/poltergeist/releases/download/v1.7.2/poltergeist-macos-universal-v1.7.2.tar.gz"
-  sha256 "63f0fab888ab783c23f1e40a2a141901e3a80abebecfdcbf50ec3cdaf4fb4297"
-  version "1.7.2"
+  url "https://github.com/steipete/poltergeist/releases/download/v1.9.0/poltergeist-macos-universal-v1.9.0.tar.gz"
+  sha256 "f51780ebc0b37bfb747167b9b2c98b5a4d30c9017becfc1a10373402c2ea4aaf"
+  version "1.9.0"
   license "MIT"
 
   def install
@@ -13,6 +13,6 @@ class Poltergeist < Formula
 
   test do
     system "#{bin}/poltergeist", "--version"
-    assert_match "1.7.2", shell_output("#{bin}/poltergeist --version")
+    assert_match "1.9.0", shell_output("#{bin}/poltergeist --version")
   end
 end
